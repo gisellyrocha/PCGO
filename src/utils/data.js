060f890215b0a4928a4283cb2b2b830bcb673e55
GisellyRock <gisellydsr@gmail.com>
2023-12-06 08:57:35 -0300

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,157 @@
+import { HiOutlineDesktopComputer } from 'react-icons/hi';
+import { CiMobile1 } from 'react-icons/ci';
+import { MdWorkspacesOutline } from 'react-icons/md';
+export const projectExperience = [
+  {
+    name: 'Website Design',
+    projects: 76,
+    icon: HiOutlineDesktopComputer,
+    bg: '#8ACDD7',
+  },
+  {
+    name: 'Mobile App Design',
+    projects: 63,
+    icon: CiMobile1,
+    bg: '#FF90BC',
+  },
+  {
+    name: 'Brand Identity',
+    projects: 47,
+    icon: MdWorkspacesOutline,
+    bg: '#F9F9E0',
+  },
+];
+
+export const WhatDoIHelp = [
+  'Sou formada pela faculdade xyz há 15 anos fui aprovada no concurso abc, e passei a atender pessoas jls',
+  'Fiz pós graduaçao em harmonizaçao facial e atualmente trabalho além de sdj faço aplicaçao de botox',
+  'Atualmente embelezo minhas pacientes aplicando beleza e autoestima',
+];
+
+export const workExp = [
+  {
+    place: 'Self-Employed, Brisbane',
+    tenure: 'Aug 2014 - Sep 2016',
+    role: 'Visual Designer',
+    detail:
+      'A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts',
+  },
+  {
+    place: 'New Man Services',
+    tenure: 'Aug 2014 - Sep 2016',
+    role: 'UI/UX Designer',
+    detail:
+      'A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts',
+  },
+  {
+    place: 'Global Solution',
+    tenure: 'Aug 2014 - Sep 2016',
+    role: 'Sr. Product Designer',
+    detail:
+      'A visual desginer dsesign for a variety of platoforms, may include internet and internet sites, games, movies, kioasks and wearbies. In short, they create the concepts',
+  },
+];
+
+export const comments = [
+  {
+    name: 'Anamika Sandula',
+    post: 'Creative Manager',
+    comment:
+      'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    img: './people2.png',
+  },
+  {
+    name: 'Anamika Sandula',
+    post: 'Creative Manager',
+    comment:
+      'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    img: './people1.png',
+  },
+  {
+    name: 'Anamika Sandula',
+    post: 'Creative Manager',
+    comment:
+      'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    img: './people2.png',
+  },
+  {
+    name: 'Anamika Sandula',
+    post: 'Creative Manager',
+    comment:
+      'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    img: './people1.png',
+  },
+  {
+    name: 'Anamika Sandula',
+    post: 'Creative Manager',
+    comment:
+      'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    img: './people2.png',
+  },
+  {
+    name: 'Anamika Sandula',
+    post: 'Creative Manager',
+    comment:
+      'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    img: './people1.png',
+  },
+  {
+    name: 'Anamika Sandula',
+    post: 'Creative Manager',
+    comment:
+      'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    img: './people2.png',
+  },
+  {
+    name: 'Anamika Sandula',
+    post: 'Creative Manager',
+    comment:
+      'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    img: './people1.png',
+  },
+  {
+    name: 'Anamika Sandula',
+    post: 'Creative Manager',
+    comment:
+      'Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    img: './people2.png',
+  },
+];
+
+export const sliderSettings = {
+  dots: true,
+  infinite: false,
+  speed: 1000,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  initialSlide: 0,
+  touchMove: true,
+  useCSS: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
