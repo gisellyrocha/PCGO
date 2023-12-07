b3d5932da6b7990b3bbcf5febe0a4eb45a8c74e7
GisellyRock <gisellydsr@gmail.com>
2023-12-07 19:47:27 -0300

    feat: footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,43 +1,46 @@
-import React from "react";
-import { footerVariants, staggerChildren } from "../../utils/motion";
-import css from "./Footer.module.scss";
-import {motion} from 'framer-motion'
+import React from 'react';
+import { footerVariants, staggerChildren } from '../../utils/motion';
+import css from './Footer.module.scss';
+import { motion } from 'framer-motion';
+
 const Footer = () => {
   return (
     <motion.section
-    variants={staggerChildren}
-    initial="hidden"
-    whileInView="show"
-    viewport={{ once: false, amount: 0.25 }}
-    className={`paddings ${css.wrapper}`}>
-      
-      
+      variants={staggerChildren}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`paddings ${css.wrapper}`}
+    >
       <motion.div
-      variants={footerVariants}
-      className={`innerWidth yPaddings flexCenter ${css.container}`}>
+        variants={footerVariants}
+        className={`innerWidth yPaddings flexCenter ${css.container}`}
+      >
         <div className={css.left}>
           <span className="primaryText">
-            Let's make something <br />
-            amazing together.
-          </span>
-          <span className="primaryText">
-            Start by <a href="mailto:zainkeepscode@gmail.com">saying hi</a>
+            Venha hoje mesmo <br /> e conheça a nossa clínica.
           </span>
         </div>
 
         <div className={css.right}>
           <div className={css.info}>
-            <span className="secondaryText">Information</span>
-            <p>145 New York, FL 5467, USA</p>
+            <span className="secondaryText">Mais Informações:</span>
+            <p>Rua New York, Caxias, São Luiz/MA</p>
+            <br></br>
+            <p>Fone: 93 9880-5234</p>
           </div>
           <ul className={css.menu}>
-            <li>Services</li>
-            <li>Works</li>
-            <li>Notes</li>
-            <li>Experience</li>
+            <li>História</li>
+            <li>Serviços</li>
+            <li>Resultados</li>
+            <li>Feedbacks</li>
           </ul>
         </div>
       </motion.div>
+      {/* Adicionando a mensagem no final da página */}
+      <p className={css.footerMessage}>
+        <span className={css.heart}>s2</span> Giselly Rocha - Newstech - 2024
+      </p>
     </motion.section>
   );
 };
