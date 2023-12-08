f23c57e5c529124912239e692870cbd8593553c6
GisellyRock <gisellydsr@gmail.com>
2023-12-08 10:55:26 -0300

    Fix: header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import css from './Header.module.scss';
-import { BiPhoneCall, BiMenuAltRight } from 'react-icons/bi';
+import { BiMenuAltRight } from 'react-icons/bi';
 import { motion } from 'framer-motion';
 import { getMenuStyles, headerVariants } from '../../utils/motion';
 import useOutsideAlerter from '../../hooks/useOutsideAlerter';
